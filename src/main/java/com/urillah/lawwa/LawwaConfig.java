e4921ad96db23585f898e46b4e735e9cc489ccf2
Halliru Ibrahim <Urillah@gmail.com>
2020-12-01 05:23:51 +0800

    heruko config

--- src/main/java/com/urillah/lawwa/LawwaConfig.java
+++ src/main/java/com/urillah/lawwa/LawwaConfig.java
@@ -2,7 +2,10 @@ package com.urillah.lawwa;
 
 import java.io.IOException;
 
+import javax.sql.DataSource;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,28 +14,40 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class LawwaConfig implements WebMvcConfigurer {
 
 	@Bean
 	public ModelMapper modelMapper() {
-	    return new ModelMapper();
+		return new ModelMapper();
 	}
-	//used for the model mapping function - model to DTO and vice versa
-	
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	// used for the model mapping function - model to DTO and vice versa
 
-      registry.addResourceHandler("/**/*")
-        .addResourceLocations("classpath:/static/")
-        .resourceChain(true)
-        .addResolver(new PathResourceResolver() {
-            @Override
-            protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                Resource requestedResource = location.createRelative(resourcePath);
-                return requestedResource.exists() && requestedResource.isReadable() ? requestedResource : new ClassPathResource("/static/index.html");
-            }
-        });
-    }
-    
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/**/*").addResourceLocations("classpath:/static/").resourceChain(true)
+				.addResolver(new PathResourceResolver() {
+					@Override
+					protected Resource getResource(String resourcePath, Resource location) throws IOException {
+						Resource requestedResource = location.createRelative(resourcePath);
+						return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+								: new ClassPathResource("/static/index.html");
+					}
+				});
+	}
+
 }
