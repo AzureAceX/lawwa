0383e05620836de62cb3db61b845c7f968534147
Halliru Ibrahim <Urillah@gmail.com>
2020-12-01 17:02:23 +0800

    test heruko

--- src/main/resources/static/script/service.js
+++ src/main/resources/static/script/service.js
@@ -6,7 +6,7 @@ angular.module("myApp").factory("LawwaService", function ($http) {
   var listRegistry = function (cb) {
     $http({
       method: "GET",
-      url: "http://localhost:8080/registry/list",
+      url: "http://localhost:8761/registry/list",
     })
       .success(function (data, status, headers, config) {
         cb(null, data);
@@ -50,7 +50,7 @@ angular.module("myApp").factory("LawwaService", function ($http) {
   var listEmployees = function (cb) {
     $http({
       method: "GET",
-      url: "http://localhost:8080/employee/list",
+      url: "http://localhost:8761/employee/list",
     })
       .success(function (data, status, headers, config) {
         cb(null, data);
@@ -88,107 +88,6 @@ angular.module("myApp").factory("LawwaService", function ($http) {
   };
 
 
-//   var getRecipes = function (cb) {
-//     $http({
-//       method: "GET",
-//       url: "http://localhost:8080/cookrecipe/category",
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         cb(data);
-//       });
-//   };
-
-//   var postRecipe = function (cb, data) {
-//     $http({
-//       method: "POST",
-//       url: "http://localhost:8080/cookrecipe/recipe",
-//       data: data,
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         cb(data);
-//       });
-//   };
-
-//   var putRecipe = function (data, cb) {
-//     $http({
-//       method: "PUT",
-//       url: "http://localhost:8080/cookrecipe/recipe",
-//       data: data,
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         cb(data);
-//       });
-//   };
-
-//   var deleteRecipe = function (id, cb) {
-//     $http({
-//       method: "DELETE",
-//       url: "http://localhost:8080/cookrecipe/recipe/" + id,
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         cb(data);
-//       });
-//   };
-
-//   var deleteIngs = function (iid, cb) {
-//     $http({
-//       method: "DELETE",
-//       url: "http://localhost:8080/cookrecipe/recipe/ingredient/" + iid,
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         cb(data);
-//       });
-//   };
-
-//   var deleteSteps = function (sid, cb) {
-//     $http({
-//       method: "DELETE",
-//       url: "http://localhost:8080/cookrecipe/recipe/step/" + sid,
-//     })
-//       .success(function (data, status, headers, config) {
-//         // this callback will be called asynchronously
-//         // when the response is available
-//         cb(null, data);
-//       })
-//       .error(function (data, status, headers, config) {
-//         // called asynchronously if an error occurs
-//         // or server returns response with an error status.
-//         cb(data);
-//       });
-//   };
 
   return {
     listRegistry: listRegistry,
@@ -199,11 +98,5 @@ angular.module("myApp").factory("LawwaService", function ($http) {
 	getEmployeeAccount: getEmployeeAccount,
 	createEmployee: createEmployee,
 
-    // getRecipes: getRecipes,
-    // postRecipe: postRecipe,
-    // putRecipe: putRecipe,
-    // deleteRecipe: deleteRecipe,
-    // deleteIngs: deleteIngs,
-    // deleteSteps: deleteSteps,
   };
 });
