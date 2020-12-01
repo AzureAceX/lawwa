f37eb042839d1738834e08748528d7ba75c3b10f
Halliru Ibrahim <Urillah@gmail.com>
2020-12-02 06:07:44 +0800

    port revert

--- src/main/resources/static/script/service.js
+++ src/main/resources/static/script/service.js
@@ -6,7 +6,7 @@ angular.module("myApp").factory("LawwaService", function ($http) {
   var listRegistry = function (cb) {
     $http({
       method: "GET",
-      url: "http://localhost:8080/registry/list",
+      url: "http://localhost:8761/registry/list",
     })
       .success(function (data, status, headers, config) {
         cb(null, data);
